f9992b8896c43d632b5402e053278a328c0d887e
Pete <peterwbrodie@gmail.com>
2021-05-25 13:35:08 +0100

    MVP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+
+const [inputNumber, setInputNumber] = useState(1);
+const [answer, setAnswer] = useState("");
+const [multiNumber, setMultiNumber] = useState(10);
+
+const handleIncrease = () => {
+  setInputNumber(inputNumber + 1);
+}
+const handleMulti = () => {
+  let randomNumber = Math.floor(Math.random())
+  setMultiNumber(randomNumber * 10)
+
+
+
+
+
+useEffect(() => {
+  
+    if (inputNumber % 3 === 0 && inputNumber % 5 === 0) {
+      setAnswer("fizzbuz")
+    } else if (inputNumber % 3 === 0) {
+      setAnswer("fizz")
+    } else if (inputNumber % 5 === 0) {
+      setAnswer("buzz")
+    } else {
+      setAnswer(inputNumber);
+    }
+  }, [inputNumber])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>FIZZBUZZ</h1>
+      <button value={inputNumber} onClick={handleIncrease}> + </button>
+      <button value={multiNumber} onClick={handleMulti}> x </button>
+      
+      <h2>Answer is {answer}</h2>
+      <h2>Multi number is {multiNumber}</h2>
     </div>
   );
 }
